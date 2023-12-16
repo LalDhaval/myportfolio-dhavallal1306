6086ab69ce7f6ad053d3819b867d221cfbe40724
LalDhaval <dhavallal787@gmail.com>
2023-12-16 12:04:55 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,11 @@ $(document).ready(function(){
     });    
 
 });
+
+var typed = new Typed(".multiple-text",{
+    strings:["Web Developer","Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
